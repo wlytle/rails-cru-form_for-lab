2240318e27ea214a0cc364ad0970d34e2f5a3f9a
wlytle <wlytle89@gmail.com>
2020-10-29 07:45:27 -0700

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,34 @@
+class GenresController < ApplicationController
+  def show
+    get_genre
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(get_params)
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    get_genre
+  end
+
+  def update
+    get_genre
+    @genre.update(get_params)
+    redirect_to genre_path(@genre)
+  end
+
+  private
+
+  def get_genre
+    @genre = Genre.find(params[:id])
+  end
+
+  def get_params
+    params.require(:genre).permit(:name)
+  end
+end
